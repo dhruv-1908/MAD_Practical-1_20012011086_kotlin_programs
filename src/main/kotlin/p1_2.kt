595ab0c61070b262c3cdc35dbab9a41e7bb0bef8
dhruv <dhruvpatel.dpdp.2002@gmail.com>
2022-08-23 19:53:58 +0530

    initial commit

---
+++ src/main/kotlin/p1_2.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val n:Int =86
+    val d:Double=n.toDouble()
+    println("Integer value :$n")
+    println("Double value:$d" )
+
+    val s: String ="86"
+    val i:Int = s.toInt()
+    println("String value:$s")
+    println("Double value (To string):$i")
+
+    val d1: Double = s.toDouble()
+    println("Double value (To String): $d1")
+}
