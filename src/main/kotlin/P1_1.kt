cae4c04fd02de9726e8ad26179ebbac00ac41f6f
dhruv <dhruvpatel.dpdp.2002@gmail.com>
2022-08-23 19:37:25 +0530

    initial commit

---
+++ src/main/kotlin/P1_1.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+
+    val a :Int =10
+    val b : Float = 1.5F
+    var c : String = "Dhruv"
+    var d : Char = 'D'
+    val e : Double = 10.5
+    val f : Boolean = false
+    val g : Long = 456212447789
+    val h : Short = -4
+    val i : Byte =123
+
+    println("Integer value: $a")
+    println("Float value : $b")
+    println("String value: $c")
+    println("Character value:$d")
+    println("Double value:$e")
+    println("Boolean value:$f")
+    println("Long value:$g")
+    println("Short value:$h")
+    println("Byte value:$i")
+}
